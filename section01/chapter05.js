4bf741103fd904fc65ef6a36534a456387cbb086
gun1kim <obkh31@naver.com>
2025-02-10 23:46:16 +0900

    ~section01 chapter06 fin

---
+++ section01/chapter05.js
@@ -0,0 +1,26 @@
+// 1. Number Type
+let num1 = 26;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity
+let mInf = -Infinity
+
+// 2. String Type
+let myName = "김건일";
+let myLocation = "경기광주";
+let introduce = myName + myLocation;
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
